f9f107efa5c114a78229486d99201b9aa2b629b1
inshal ayaz <inshal.ayaz14@gmail.com>
2021-05-30 19:53:23 +0500

    insert posts into database

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -11,6 +11,18 @@ class User{
     }
 
 
+
+    public function getUserName(){
+        return $this->user['username'];
+    }
+
+    public function getNumPosts(){
+        $username = $this->user['username'];
+        $query = mysqli_query($this->con, " SELECT num_posts FROM users WHERE username='$username' ");
+        $row = mysqli_fetch_array($query);
+        return $row['num_posts'];
+    }
+
     public function getFirstAndLastName(){
         $username = $this->user['username'];
         $query = mysqli_query($this->con," SELECT first_name, last_name FROM users WHERE username='$username' ");
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php 
 include './includes/header.php';
-include './includes/classes/User.php'
+include './includes/classes/User.php';
+include './includes/classes/Post.php';
+
+
+if( isset($_POST['post']) ){
+	$post = new Post($con, $userLoggedIn);
+	$post->submitPost($_POST['post_text'], 'none');
+}
+
+
 ?>
 	
 <div class="user_details column" >
